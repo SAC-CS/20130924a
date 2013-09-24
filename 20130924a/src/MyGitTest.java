aca9ee18e017e61770ce01589d1e67c1728bad65
SAC-CS <huynh_durendal@sac.edu>
2013-09-24 00:29:23 -0700

    Add v2

--- 20130924a/src/MyGitTest.java
+++ 20130924a/src/MyGitTest.java
@@ -3,7 +3,7 @@ public class MyGitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("Git test 2013-09-24a");
+		 System.out.println("Git test 2013-09-24a v2");
 	}
 
 }
